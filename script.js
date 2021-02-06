5cfb5104e7af54243d28b4da08140dba85b61e8e
Mateus Gomes <44241134+mateusfranca97@users.noreply.github.com>
2021-02-06 19:54:41 -0300

    0.0.4

--- script.js
+++ script.js
@@ -26,6 +26,12 @@ const transaction = [
         amount: -20000,
         date: '23/01/2021'
     },
+    {
+        id: 4,
+        description: 'App',
+        amount: 200000,
+        date: '23/01/2021'
+    },
 ]
 
 const Transaction = {
@@ -41,21 +47,47 @@ const Transaction = {
 }
 
 const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
     addTransaction(transaction,index){
-        console.log(transaction)
         const tr = document.createElement('tr')
         tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        
+        DOM.transactionsContainer.appendChild(tr)
+    
     },
-    innerHTMLTransaction(){
-         const html =
-         `
-            <td class="description">${transaction.id}</td>
-            <td class="expense">${transaction.description}</td>
+    innerHTMLTransaction(transaction){
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
+        const html =
+        `
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td><img src="assets/minus.svg" alt="Remover Transação"></td>
-        `
-        return html
+            `
+            return html
      }
 } 
 
-DOM.addTransaction(transaction[0])
+const Utils = {
+    formatCurrency(value){
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, "")
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-BR",{
+            style: "currency",
+            currency:"BRL"
+        })
+        return signal + value
+    }
+}
+
+transaction.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})
